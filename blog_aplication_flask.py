644fbe8909f7a8d386572a0f29d03dd56c534f79
ali al-kheder <alkhdyyr@gmail.com>
2025-03-03 21:23:56 +0100

    create the template and the indexing

--- blog_aplication_flask.py
+++ blog_aplication_flask.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+blog_posts = [
+    {
+        'id':1,
+        'title': 'First Post',
+        'content': 'This is my first post. It is very short.',
+        'author': 'John Doe'
+    },
+    {
+        'id':2,
+        'title': 'Second Post',
+        'content': 'This is my second post. It is a bit longer.',
+        'author': 'Jane Smith'
+    },
+    {
+        'id':3,
+        'title': 'Third Post',
+        'content': 'This is my third post. It is the longest.',
+        'author': 'John Doe'
+    }
+]
+
 @app.route('/')
-def hello_world():
-    print('Hello, World!'
-    )
-    return 'Hello, World!'
+def index():
+
+    return render_template('index.html',posts=blog_posts)
 
 
 
@@ -22,4 +42,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=3000)
+    app.run(host="0.0.0.0",debug=True,port=3000)
